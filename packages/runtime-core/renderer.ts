7c64ac038d454d5947268914fe9ef16693cfd93d
HiroshiIgarashi <tubukiti2008@gmail.com>
2025-12-07 21:21:51 +0900

    リファクタリング

---
+++ packages/runtime-core/renderer.ts
@@ -0,0 +1,23 @@
+export interface RendererOptions<HostNode = RendererNode>{
+  setElementText(node: HostNode, text:string): void
+}
+
+export interface RendererNode {
+  [key: string]: any
+}
+
+export interface RendererElement extends RendererNode { }
+
+export type RootRendererFunction<HostElement = RendererElement> = (
+  message: string,
+  container:HostElement
+) => void 
+
+export function createRenderer(options: RendererOptions)  {
+  const {setElementText: hostSetElementText} = options
+
+  const render: RootRendererFunction = (message, container) => {
+    hostSetElementText(container,message)
+  }
+  return { render }
+}
---
+++ packages/runtime-dom/index.ts
@@ -0,0 +1,18 @@
+import { createAppAPI, CreateAppFunction } from "../runtime-core/apiCreateApp";
+import { createRenderer } from "../runtime-core/renderer";
+import { nodeOps } from "./nodeOps";
+
+const { render } = createRenderer(nodeOps)
+const _createApp = createAppAPI(render)
+
+export const createApp = ((...args) => {
+  const app = _createApp(...args)
+  const { mount } = app
+  app.mount = (selector: string) => {
+    const container = document.querySelector(selector)
+    if (!container) return
+    mount(container)
+  }
+
+  return app
+}) as CreateAppFunction<Element>
