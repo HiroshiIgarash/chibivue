50ca08ad1d48933307815c506c6495dc2f3245be
HiroshiIgarashi <tubukiti2008@gmail.com>
2025-12-07 22:11:15 +0900

    h関数の実装

--- packages/runtime-core/apiCreateApp.ts
+++ packages/runtime-core/apiCreateApp.ts
@@ -1,25 +1,25 @@
-import { Component } from "./component"
-import { RootRendererFunction } from "./renderer"
+import { Component } from "./component";
+import { RootRendererFunction } from "./renderer";
 
 export interface App<HostElement = any> {
-  mount(rootContainer:HostElement|string):void
+  mount(rootContainer: HostElement | string): void;
 }
 
 export type CreateAppFunction<HostElement> = (
-  rootComponent:Component
-) => App<HostElement>
+  rootComponent: Component
+) => App<HostElement>;
 
 export function createAppAPI<HostElement>(
-  render:RootRendererFunction<HostElement>
-): CreateAppFunction<HostElement>{
+  render: RootRendererFunction<HostElement>
+): CreateAppFunction<HostElement> {
   return function createApp(rootComponent) {
     const app: App = {
       mount(rootContainer: HostElement) {
-        const message = rootComponent.render!()
-        render(message,rootContainer)
-      }
-    }
+        const vnode = rootComponent.render!();
+        render(vnode, rootContainer);
+      },
+    };
 
-    return app
-  }
+    return app;
+  };
 }
--- packages/runtime-core/renderer.ts
+++ packages/runtime-core/renderer.ts
@@ -1,23 +1,48 @@
-export interface RendererOptions<HostNode = RendererNode>{
-  setElementText(node: HostNode, text:string): void
+import { VNode } from "./vnode";
+
+export interface RendererOptions<HostNode = RendererNode> {
+  createElement(type: string): HostNode;
+
+  createText(text: string): HostNode;
+
+  setElementText(node: HostNode, text: string): void;
+
+  insert(child: HostNode, parent: HostNode, anchor?: HostNode | null): void;
 }
 
 export interface RendererNode {
-  [key: string]: any
+  [key: string]: any;
 }
 
-export interface RendererElement extends RendererNode { }
+export interface RendererElement extends RendererNode {}
 
 export type RootRendererFunction<HostElement = RendererElement> = (
   message: string,
-  container:HostElement
-) => void 
+  container: HostElement
+) => void;
 
-export function createRenderer(options: RendererOptions)  {
-  const {setElementText: hostSetElementText} = options
+export function createRenderer(options: RendererOptions) {
+  const {
+    createElement: hostCreateElement,
+    createText: hostCreateText,
+    insert: hostInsert,
+  } = options;
 
-  const render: RootRendererFunction = (message, container) => {
-    hostSetElementText(container,message)
+  function renderVNode(vnode: VNode | string) {
+    if (typeof vnode === "string") return hostCreateText(vnode);
+    const el = hostCreateElement(vnode.type);
+
+    for (const child of vnode.children) {
+      const childEl = renderVNode(child);
+      hostInsert(childEl, el);
+    }
+
+    return el;
   }
-  return { render }
+
+  const render: RootRendererFunction = (vnode, container) => {
+    const el = renderVNode(vnode);
+    hostInsert(el, container);
+  };
+  return { render };
 }
